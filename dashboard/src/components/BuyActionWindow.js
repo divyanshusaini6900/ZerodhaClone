0f692b8c023074888ebb01a597ae0ee4e5c2f23c
Divyanshu Saini <ads60g@gmail.com>
2025-06-15 23:29:11 +0530

    Update BuyActionWindow.js
    localhost link change into api

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -12,7 +12,7 @@ const BuyActionWindow = ({ uid }) => {
   const [stockPrice, setStockPrice] = useState(0.0);
 
   const handleBuyClick = () => {
-    axios.post("http://localhost:3002/newOrder", {
+    axios.post("https://zerodhabackend-4tzw.onrender.com/allHoldings/newOrder", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,

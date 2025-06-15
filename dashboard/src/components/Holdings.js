da5bfc11979a4147d2ddec8805201d648a8363a0
Divyanshu Saini <ads60g@gmail.com>
2025-06-15 23:29:40 +0530

    Update Holdings.js

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -8,7 +8,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3002/allHoldings").then((res) => {
+    axios.get("https://zerodhabackend-4tzw.onrender.com/allHoldings/allHoldings").then((res) => {
       // console.log(res.data);
       setAllHoldings(res.data);
     });
